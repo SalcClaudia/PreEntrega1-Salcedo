0ba50060a8247e2afd2fec56bb7242de7bf675da
Claudia Salcedo <csprojectsinfo03@gmail.com>
2024-04-16 18:09:27 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let petSelection;
+
+alert("Bienvenido o bienvenida a Find Me A Pet, donde te hacemos match con tu mejor amigo");
+
+let welcomeUser = prompt("¿Podría escribir su nombre?");
+
+
+if (welcomeUser) {
+    let petSelection = parseInt(prompt("Bienvenid@ " + welcomeUser + ". ¿Qué tipo de compañero estás buscando? Elige un número de la lista: \n\n1. Perro\n2. Gato\n3. Loro"));
+
+    switch (petSelection.toLowerCase) {
+        case 1:
+            alert("El mejor amigo del hombre. Tenemos desde pequeños senior hasta cachorros");
+            break
+        case 2:
+            alert("gato");
+            break
+        case 3:
+            alert("el loro");
+            break
+        default:
+            alert("Lo sentimos, por los momentos no tenemos a ese companero en adopcion");
+            break
+    }
+
+
+};*/
+
+
