11473646c51ac5d4280500751007ae03c2da4184
Claudia Salcedo <csprojectsinfo03@gmail.com>
2024-04-17 17:29:22 -0500

    Se ha resuelto el bug de los decimales en el resultado

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
 
-/*function celsiusToFahrenheit() {
-    const celsius = (celsius * 9 / 5) + 32;
-    alert("Hoy estamos a" + " " + firstTemp + "ºC");
-
-}
-
-function fahrenheitToCelsius() {
-    const fahrenheit = (fahrenheit - 32) * 5 / 9;
-    alert("Hoy estamos a" + " " + secondTemp + "ºF");
-
-}*/
 
 function temperature1 (celsius){
-    alert("Hoy estamos a" + " " + (celsius * 9 / 5) + 32 + "ºC");
+    let celsiusToFahrenheit = (celsius * 9/5) + 32;
+    alert("Hoy estamos a" + " " + celsiusToFahrenheit + "ºF");
 }
 
 function temperature2 (fahrenheit){
-    alert("Hoy estamos a" + " " + (fahrenheit - 32) * 5 / 9 + "ºF");
+    let fahrenheitToCelsius = (fahrenheit - 32) * 5/9;
+    alert("Hoy estamos a" + " " + fahrenheitToCelsius + "ºC");
 }
 
 alert("Se bienvenido al conversor de temperatura ºC a ºF");
 
-let tempSelection = parseInt(prompt("Por favor escoja de la lista la opción que desea convertir: \n\.1 Celsius \n\.2 Fahrenheit \n\.0 Salir"));
+let tempSelection = parseInt(prompt("Por favor escoja de la lista la opción que desea convertir: \n\.1 Celsius a Fahrenheit \n\.2 Fahrenheit a Celsius \n\.0 Salir"));
 
 switch (tempSelection) {
     case 0:
@@ -34,7 +25,7 @@ switch (tempSelection) {
         
         break;
         case 2:
-            let fahrenheit
+            let fahrenheit;
             fahrenheit = parseInt(prompt("Ingrese su cantidad en Fahrenheit"));
             temperature2 (fahrenheit);
         break;
@@ -44,53 +35,10 @@ switch (tempSelection) {
         break;
 }
 
-/*alert("Se bienvenido al conversor de temperatura ºC a ºF")
-
-const tempSelection = parseInt(prompt("Por favor escoja de la lista la opción que desea convertir: \n\.1 Celsius \n\.2 Fahrenheit \n\.0 Salir"));
-*/
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*let petSelection;
-
-alert("Bienvenido o bienvenida a Find Me A Pet, donde te hacemos match con tu mejor amigo");
-
-let welcomeUser = prompt("¿Podría escribir su nombre?");
-
-
-if (welcomeUser) {
-    let petSelection = parseInt(prompt("Bienvenid@ " + welcomeUser + ". ¿Qué tipo de compañero estás buscando? Elige un número de la lista: \n\n1. Perro\n2. Gato\n3. Loro"));
-
-    switch (petSelection.toLowerCase) {
-        case 1:
-            alert("El mejor amigo del hombre. Tenemos desde pequeños senior hasta cachorros");
-            break
-        case 2:
-            alert("gato");
-            break
-        case 3:
-            alert("el loro");
-            break
-        default:
-            alert("Lo sentimos, por los momentos no tenemos a ese companero en adopcion");
-            break
-    }
-
-
-};*/
 
 
